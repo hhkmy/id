265c635ff825992503b03a048537784fc9fbe289
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2025-04-13 16:24:07 +0630

    tailwind: config update
    update tailwind config for hugo_stats

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./hugo_stats.json'],
+  darkMode: "class",
+  content: ["./hugo_stats.json", "./**/*.html", "./content/**/*.md"],
   theme: {
     extend: {
       screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '1100px',
-        xl: '1440px',
+        sm: "480px",
+        md: "768px",
+        lg: "1100px",
+        xl: "1440px",
       },
     },
   },
