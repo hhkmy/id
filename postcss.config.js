ad9d201607342883be59e747e64c9b53046d40bb
Hein Htet Kyaw <mr.kiddomonster@gmail.com>
2025-04-15 18:42:01 +0630

    wtf: ❓
    don't know

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
-let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
-const tailwind = require('tailwindcss')(tailwindConfig);
-const autoprefixer = require('autoprefixer');
+let tailwindConfig =
+    process.env.HUGO_FILE_TAILWIND_CONFIG_JS || "./tailwind.config.js";
+const tailwind = require("tailwindcss")(tailwindConfig);
+const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
+    plugins: [
+        tailwind,
+        ...(process.env.HUGO_ENVIRONMENT === "production"
+            ? [autoprefixer]
+            : []),
+    ],
 };
