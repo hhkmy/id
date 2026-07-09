53118cfd2c35c4950abafbba978f917fe471f2bf
Hein Htet Kyaw <me@hhk.my.id>
2026-07-09 17:59:31 +0630

    fix(js): avoid html injection sink

--- assets/js/article-list-icons.js
+++ assets/js/article-list-icons.js
@@ -15,8 +15,20 @@ export function initArticleListIcons() {
       icon.setAttribute("height", "24");
       icon.setAttribute("fill", "none");
       icon.setAttribute("viewBox", "0 0 24 24");
-      icon.innerHTML =
-        '<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>';
+
+      const path = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "path",
+      );
+      path.setAttribute("stroke", "currentColor");
+      path.setAttribute("stroke-linecap", "round");
+      path.setAttribute("stroke-linejoin", "round");
+      path.setAttribute("stroke-width", "2");
+      path.setAttribute(
+        "d",
+        "M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z",
+      );
+      icon.append(path);
 
       item.prepend(icon);
     });
